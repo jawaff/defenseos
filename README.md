fab096e057c678997ebd38dbc57ca5441ed6a240
Jake Waffle <2895324+jawaff@users.noreply.github.com/>
2024-12-23 20:56:32 -0700

    Minor documentation touch up

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # DefenseOS
 
 ## Prerequisites
+
 - Java 21
 
 ## Running
@@ -17,4 +18,10 @@
 .\gradlew.bat uberJar
 
 java -jar defenseos-sawtooth\build\libs\defenseos-sawtooth-1.0-SNAPSHOT-uber.jar
+```
+
+## Generating Javadocs
+
+```
+.\gradlew.bat javadoc
 ```
--- defenseos-api/src/main/java/com/dzyne/defenseos/api/radar/IRadar.java
+++ defenseos-api/src/main/java/com/dzyne/defenseos/api/radar/IRadar.java
@@ -9,6 +9,11 @@ public interface IRadar {
      * Registers a consumer on our radar so that our radars are able to know who to publish radar detection events to.
      * This strategy allows for our consumers to not have direct knowledge of every radar implementation and allows
      * the radars to have no knowledge of the consumers.
+     * <br>
+     * I do now see a way to connect Kafka or RabbitMQ to Spring so that we can use them for the producer-consumer
+     * strategy. This is what I went with off the top of my head because it's simple, but obviously this only works
+     * within a single process.
+     *
      * @param radarDetectionConsumer A consumer function that will take in a radar detection event.
      */
     void registerRadarDetectionConsumer(final Consumer<RadarDetection> radarDetectionConsumer);
